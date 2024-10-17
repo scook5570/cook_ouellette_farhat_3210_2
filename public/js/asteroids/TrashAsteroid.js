e8d53271f79da4b8689d85f6a8c4d51ab11c1e76
SOuellette22 <143441012+SOuellette22@users.noreply.github.com>
2024-10-17 15:08:37 -0400

    feat: added the set up for the trash object

---
+++ public/js/asteroids/TrashAsteroid.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+export default class TrashAsteroid {
+    constructor() {
+        
+    }
+
+    // This function updates the asteroid movement through space
+    update(t) {
+        
+    }
+}
