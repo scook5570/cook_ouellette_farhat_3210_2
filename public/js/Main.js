e75689b684cc9f6e75241e4f23e036cb768d88d8
scook5570 <cooksm@merrimack.edu>
2024-10-19 12:54:51 -0400

    feat: cockpit loads and is bound to camera movement

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,16 +1,17 @@
 import * as THREE from "three";
 import ObjectPool from "./ObjectPool";
 import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(
-  35,
+  65,
   window.innerWidth / window.innerHeight,
   0.1,
   3000
 );
-camera.position.z = 200;
+camera.position.set(0, 0, 200); // Move camera slightly in front of where the cockpit will be
 scene.add(camera);
 
 // Add PointerLockControls
@@ -21,10 +22,18 @@ renderer.setClearColor(0x000000);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Add basic lighting - Change later I just wanted to see the model
+const ambientLight = new THREE.AmbientLight(0xffffff, 1); 
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light to simulate sunlight
+directionalLight.position.set(10000, 10000, 10000); // Position way off in the distance
+scene.add(directionalLight);
+
 var pool = new ObjectPool(10).pool;
 console.log(pool);
 
-const moveSpeed = 0.5
+const moveSpeed = 0.5;
 
 for (var i = 0; i < pool.length; i++) {
   var a = pool[i];
@@ -33,6 +42,26 @@ for (var i = 0; i < pool.length; i++) {
   a.mesh.translateY(Math.random() * 50 - 25);
   a.mesh.translateZ(Math.random() * 50 - 25);
 }
+
+// Load the cockpit model
+const loader = new GLTFLoader();
+loader.load(
+  "eagle5.glb",
+  function (gltf) {
+    const cockpit = gltf.scene;
+
+    // Adjust scale and position the cockpit
+    cockpit.scale.set(0.5, 0.5, 0.5); 
+    cockpit.position.set(0.68, -1.4, -0.5); 
+
+    camera.add(cockpit); // Binds it to the camera rather than world
+    cockpit.rotation.set(0, Math.PI, 0); // Flip on y axis
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 // Pointer Lock API to capture mouse input
 function enablePointerLock() {
@@ -72,7 +101,6 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
-animate();
 
 // Resize handler to ensure correct aspect ratio
 window.addEventListener("resize", function () {
@@ -80,3 +108,5 @@ window.addEventListener("resize", function () {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
+animate();
